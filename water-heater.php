8b99eceebc21c48c546c3883a4fc06f9b1e5ef9a
Your Name <mahboobabubakar786@gmail.com>
2023-07-27 15:31:11 +0500

    updated water heater

---
+++ water-heater.php
@@ -0,0 +1,102 @@
+<?php include_once('header.php') ?>
+<title>Jacks Heating and Cooling| Water Heaters</title>
+<meta name="rating" content="general" />
+<meta name="revisit-after" content="3 days" />
+<meta name="robots" content="index, follow" />
+<meta name="classification" content=" water heater, tankless water heater, Lapeer, MI" />
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+                                <img src="<?php echo $base_url ?>/img/local water heater-1 (2).jpg" alt="water heater"
+                                    class="full-width-img">
+                                <br>
+                                <br>
+                                <strong>We provide professional services for both traditional tanked water heaters and
+                                    tankless water heaters in the
+                                    Thumb area of Michigan.</strong>
+
+                                <p>When it comes to your water heater, it's essential to rely on an expert to ensure
+                                    optimal performance and
+                                    efficiency. Whether you're considering the replacement of a traditional tanked water
+                                    heater or exploring the
+                                    option of a tankless water heater, Jacks Heating and Cooling is the ideal company
+                                    for you. We specialize in
+                                    matching you with a water heater that perfectly aligns with your hot water
+                                    requirements, offering the right
+                                    capacity, energy efficiency, and price to meet your needs.</p>
+
+                                <h2>What Is A Tankless Water Heater?</h2>
+
+                                <p>Conventional water heaters typically store a significant volume of water at a
+                                    consistently heated temperature in
+                                    a storage tank. This results in high energy consumption, particularly during periods
+                                    when hot water isn't
+                                    required. On the other hand, a tankless water heater heats water on demand,
+                                    providing hot water only when
+                                    needed. By operating solely during active use, it effectively reduces energy
+                                    consumption, leading to cost
+                                    savings on home energy expenses. Additionally, opting for a tankless water heater is
+                                    an environmentally friendly
+                                    choice for your home.</p>
+
+                                <h2>Endless Hot Water</h2>
+
+                                <p>A tankless water heater provides an endless supply of hot water as it heats water on
+                                    demand. It is an excellent
+                                    solution for homes with larger hot water needs, such as those with a tub, spa, or
+                                    pool, as well as for large
+                                    families or bigger homes. If you currently have a 40-gallon or larger tank and are
+                                    considering an upgrade, we
+                                    highly recommend considering a tankless water heater. Our team at Jacks Heating and
+                                    Cooling can send a service
+                                    technician to your home or business to conduct an inspection and provide you with
+                                    detailed cost estimates for
+                                    installing a new tankless water system.</p>
+
+                                <p>Compared to conventional tanked water heaters, tankless water heaters offer the
+                                    advantage of never running out of
+                                    hot water. With traditional water heaters, once you deplete the hot water supply,
+                                    you must wait for the entire
+                                    tank to reheat. In contrast, tankless water heaters heat water instantly as it is
+                                    needed. The size of a tankless
+                                    water heater is determined based on the flow rate and hot water usage in a home. Our
+                                    knowledgeable technicians
+                                    will ensure the perfect sizing of the tankless water heater for your home,
+                                    guaranteeing an endless supply of hot
+                                    water.</p>
+
+                                < <br>
+
+                                    <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
+                                        Quote</a>
+
+                                    <p><strong>When it comes to getting the best water heater equipment or service, the
+                                            only
+                                            name
+                                            you need to know in the Thumb
+                                            area of Michigan is Jacks Heating and Cooling.</strong> </p>
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
