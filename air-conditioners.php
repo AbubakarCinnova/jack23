d568e8ebcbc4f0ee19511b269e51d96901846d26
Your Name <mahboobabubakar786@gmail.com>
2023-07-27 13:43:28 +0500

    updated ac

---
+++ air-conditioners.php
@@ -0,0 +1,194 @@
+<?php include_once('header.php') ?>
+<title>Jack's Heating and Cooling| AC Products</title>
+<meta name="rating" content="general" />
+<meta name="revisit-after" content="3 days" />
+<meta name="robots" content="index, follow" />
+<meta name="classification"
+    content=" Furnace, Heating, Cooling, Heating &amp; Air conditioning, Heating &amp; Cooling, Air conditioning, Furnace, HVAC, Lapeer, MI" />
+<meta name="resource-type" content="document" />
+<meta name="distribution" content="Global" />
+<meta name="location" content="USA, MI, Lapeer" />
+<meta name="author" content="MW" />
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-9 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+                                <div class="thermostats-banner" style="text-align:center;"><img
+                                        src="<?php echo $base_url; ?>/img/lennox-ac (1) (1).png" alt="Air Conditionar">
+                                </div>
+
+                                <br>
+
+                                <h2>Is it the Right Time to Replace Your Air Conditioner?</h2>
+                                <p>When the hot and humid summer weather hits Michigan, you want an air conditioner that
+                                    can meet your cooling
+                                    needs. If your current central air system isn't cutting it anymore, it might be time
+                                    to consider an upgrade.
+                                    Jack's Heating and Cooling offers installation services for top-of-the-line air
+                                    conditioners and cooling
+                                    systems that are not only advanced and high-performing but also affordable and
+                                    energy-efficient. Whether it's
+                                    for your home or business, we have the perfect solution for your cooling needs.</p>
+
+                                <p>Our team of comfort designers will customize the ideal-sized system to meet your
+                                    specific needs for your home or
+                                    business. We are dedicated to providing our customers with the air conditioning
+                                    solution that suits them best,
+                                    all at reasonable and competitive prices.</p>
+
+                                <h2>What Does the SEER Rating of an AC System Mean in Simple Words?</h2>
+                                <p>In simple terms, the SEER rating of an AC system shows how well it cools your place
+                                    while saving energy. A higher
+                                    SEER rating means the AC is more efficient and can cool effectively using less
+                                    electricity. So, a higher SEER
+                                    rating is good because it saves you money on energy bills.</p>
+
+                                <h2>Variable-Capacity AC Compressor</h2>
+                                <p>The Variable-Capacity AC Compressor is a highly efficient and noise-reducing option
+                                    for air conditioning. It
+                                    operates at different speeds (35-100%) based on cooling needs, offering a wider
+                                    range of speeds compared to
+                                    two-stage systems. This allows it to run at lower speeds for longer periods and
+                                    increase to higher speeds during
+                                    hotter temperatures. By avoiding frequent on/off cycles, it maintains a continuous
+                                    and comfortable operation.
+                                    These variable-speed AC systems are the most energy-efficient among the available
+                                    options.</p>
+
+                                <h2>Two-Stage / Multi Stage AC Compressor</h2>
+                                <p>The Two-Stage / Multi-Stage AC Compressor operates at two distinct levels: a high
+                                    level for very hot days and a
+                                    low level for milder days. It combines the benefits of two units in one system,
+                                    starting at 70% capacity and
+                                    switching to 100% if needed. This results in longer, more consistent cooling and
+                                    increased efficiency as it
+                                    reduces frequent cycling. Additionally, the two-stage system can remove more
+                                    humidity than a single-stage system,
+                                    making it a versatile and effective cooling option.</p>
+
+                                <h2>Single-Stage AC Compressor</h2>
+                                <p>A single-stage AC compressor operates at a consistent full-cold speed, with the
+                                    thermostat regulating the home's
+                                    temperature by turning the AC system and compressor on and off. In Michigan, the
+                                    majority of homes with central
+                                    air conditioning utilize a single-stage compressor, which has been the norm for many
+                                    years. The latest
+                                    single-stage models incorporate advanced technology, resulting in significantly
+                                    improved energy efficiency
+                                    compared to older models with lower SEER ratings.</p>
+
+                                <h2>Customized Heating Solutions</h2>
+                                <p>Welcome to Jack's Heating and Cooling, where we specialize in creating personalized
+                                    home comfort solutions
+                                    designed just for you. We understand that each home is unique, so we carefully
+                                    consider factors like your home's
+                                    size, design, insulation, and even your family's behavior to find the perfect fit
+                                    for your needs. Our priority is
+                                    to discover the most cost-effective and energy-efficient option for you.</p>
+
+                                <p>Rest assured, we only recommend products that meet our high standards, and we take
+                                    the time to thoroughly explain
+                                    our recommendations and perform load calculations to ensure the equipment is
+                                    precisely sized for your home. Our
+                                    ultimate aim is to provide excellent customer service and build lasting
+                                    relationships with our valued customers.
+                                    With Jack's Heating and Cooling, you can expect top-notch solutions tailored to your
+                                    comfort and satisfaction.</p>
+
+                                <h2>Our Most Popular Air Conditioners</h2>
+
+                                <h2>Lennox Model SL28XCV</h2>
+                                <ul>
+                                    <li>Stages of Cooling: Variable-Capacity Operation</li>
+                                    <li>Up to 28 SEER</li>
+                                    <li>SEER2 Up to 25.8</li>
+                                    <li>ENERGY STAR®</li>
+                                    <li>Sound Level As low as 59 dBA</li>
+                                    <li>Cabinet PermaGuard Cabinet and SmartHinge Louver Design</li>
+                                    <li>R-410A refrigerant</li>
+                                    <li>Tax Credit Eligible</li>
+                                    <li>iComfort -enabled technology</li>
+                                    <li>10-Year Limited Warranty on compressor and covered components</li>
+                                </ul>
+
+                                <h2>Lennox Model EL18XCV</h2>
+                                <ul>
+                                    <li>Up to 18 SEER</li>
+                                    <li>SEER2 up to 18.8</li>
+                                    <li>Stages of Cooling Variable-Capacity Operation</li>
+                                    <li>ENERGY STAR®</li>
+                                    <li>Sound Level As low as 72 dBA</li>
+                                    <li>PermaGuard Cabinet and SmartHinge Louver Design</li>
+                                    <li>R-410A refrigerant</li>
+                                    <li>Tax Credit Eligible</li>
+                                    <li>iComfort -enabled technology</li>
+                                    <li>Elite models come with a 10-Year Limited Warranty on compressors and a 5-Year
+                                        Limited Warranty on covered
+                                        components. A 10-Year Extended Limited Warranty is available with product
+                                        registration.</li>
+                                </ul>
+
+                                <h2>Lennox Model ML18XC2</h2>
+                                <ul>
+                                    <li>Mid-efficiency, two-stage air conditioner</li>
+                                    <li>18.00 SEER and 17.8 SEER2</li>
+                                    <li>Stages of Cooling Two-Stage Operation</li>
+                                    <li>ENERGY STAR®</li>
+                                    <li>Sound Level As low as 75 dBA</li>
+                                    <li>PermaGuard Cabinet</li>
+                                    <li>R-410A refrigerant</li>
+                                    <li>Tax Credit Eligible</li>
+                                    <li>On-board Diagnostics</li>
+                                    <li>5-Year Limited Warranty on covered components including compressor. This product
+                                        may be eligible for valuable
+                                        10-Year Limited Extended Warranty coverage with product registration.</li>
+                                </ul>
+
+                                <h2>Lennox Model EL17XC1</h2>
+                                <ul>
+                                    <li>Mid-efficiency, single-stage air conditioner</li>
+                                    <li>SEER up to 18.6</li>
+                                    <li>SEER2 up to 17.4</li>
+                                    <li>ENERGY STAR®</li>
+                                    <li>Stages of Cooling Single Stage</li>
+                                    <li>Sound Level As low as 72 dBA</li>
+                                    <li>PermaGuard Cabinet and SmartHinge Louver Design</li>
+                                    <li>R-410A refrigerant</li>
+                                    <li>Tax Credit Eligible</li>
+                                    <li>Elite models come with a 10-Year Limited Warranty on compressors and a 5-Year
+                                        Limited Warranty on covered
+                                        components. A 10-Year Extended Limited Warranty is available with product
+                                        registration.</li>
+                                </ul>
+
+                                <p><strong>If you're looking for the best heating and cooling equipment or service in
+                                        the Thumb area of Michigan, the only name you need to know is Jack's Heating and
+                                        Cooling.</strong></p>
+                                <p><strong>Contact Us for a Free in-Home Estimate For a New Home Comfort System</strong>
+                                </p>
+
+                                <br>
+
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
+                                    Quote</a>
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
