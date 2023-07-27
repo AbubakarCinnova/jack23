8c9048d3ec94fe3ef76c1ccd284c6f83295fb409
Your Name <mahboobabubakar786@gmail.com>
2023-07-27 13:54:53 +0500

    updated special

---
+++ specials.php
@@ -0,0 +1,33 @@
+<?php include_once('header.php') ?>
+<title>Jack heating and cooling | Specials </title>
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page  page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 30px;">
+                    <div class=" content-area specials-page">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <img src="<?php echo $base_url ?>/img/jack's_20OFF_6x425-1 (2).png" alt="ad"
+                                    class="img-responsive">
+                            </div>
+
+
+                            <!--  <div class="col-sm-12">
+                                <br>
+                                <p>**Financing available to qualified buyers on approved credit. No down payment is required. Cannot be combined with any other promotional offer. You may prepay your account at any time without penalty. Financing is subject to credit requirements and satisfactory completion of finance documents. Any finance terms advertised are estimates only. See Truth in Lending disclosures available from the lender for more information.</p>
+                            </div> -->
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
