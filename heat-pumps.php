917db8f6db92c4be368289edcbd8ad26c662410e
Your Name <mahboobabubakar786@gmail.com>
2023-07-27 15:17:05 +0500

    Create heat-pumps.php

---
+++ heat-pumps.php
@@ -0,0 +1,141 @@
+<?php include_once('header.php') ?>
+<title>Jacks Heating and Cooling| Heat Pumps</title>
+<meta name="rating" content="general" />
+<meta name="revisit-after" content="3 days" />
+<meta name="robots" content="index, follow" />
+<meta name="classification"
+    content=" Furnace, Heat Pumps, Cooling, Heating &amp; Air conditioning, dual fuel, Furnace, HVAC" />
+<meta name="resource-type" content="document" />
+<meta name="distribution" content="Global" />
+<meta name="location" content="USA, MI, Lapeer" />
+<meta name="author" content="RW" />
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page  page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 30px;">
+                    <div class=" content-area specials-page">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h1>Understanding Heat Pumps</h1>
+                                <h2>A Dual-Function Heating and Cooling Solution for Michigan Homes</h2>
+
+                                <p>In Michigan, a heat pump proves to be a highly versatile option for homeowners,
+                                    functioning both as a heating and
+                                    cooling system. By combining a heat pump with a furnace, you create a dual fuel
+                                    system that guarantees
+                                    year-round comfort. During the summer, the heat pump operates as an air conditioner,
+                                    while in the winter, it
+                                    efficiently extracts heat from the outdoor air and transfers it indoors to keep your
+                                    home warm and cozy.</p>
+
+                                <p>To achieve peak performance, a heat pump operates most efficiently when temperatures
+                                    are above 35 degrees
+                                    Fahrenheit. In colder weather, a furnace becomes the primary heating source. In
+                                    regions like Michigan, where
+                                    temperatures can significantly drop, homeowners may require an additional heating
+                                    solution. However, by combining
+                                    a Lennox heat pump with a highly efficient furnace, it is possible to establish an
+                                    energy-saving dual-fuel
+                                    system. This system reduces the consumption of natural gas and guarantees optimal
+                                    comfort even during extremely
+                                    cold days.</p>
+
+                                <p>Energy efficiency is impacted by numerous factors, encompassing HVAC system design,
+                                    size, and maintenance, as
+                                    well as the local climate, building insulation, and occupant behavior. At Jacks
+                                    Heating and Cooling, our team of
+                                    comfort designers specializes in conducting comprehensive analyses and load
+                                    calculations to aid in selecting
+                                    the ideal HVAC system for residential and commercial properties. Through careful
+                                    consideration of these crucial
+                                    factors, we ensure enhanced energy efficiency and comfort for your home or business.
+                                </p>
+
+                                <p><strong>The amended Energy Efficient Home Improvement Credit, which began in 2023 and
+                                        extends
+                                        through 2032, increases the
+                                        tax credits as high as $600 for qualified air conditioners or gas furnaces, and
+                                        up
+                                        to $2000 for qualified heat
+                                        pumps, heat pump water heaters, or boiler.</strong></p>
+
+                                <p>Jacks Heating and Cooling is dedicated to assisting customers in maximizing their
+                                    credits for new heat pump
+                                    installations. By leveraging the Federal Inflation Reduction Act alongside local
+                                    Utility Rebates and
+                                    manufacturer incentives, we actively work with our customers to ensure they receive
+                                    the highest possible credits
+                                    for their purchases.</p>
+
+                                <h2>Lennox Model SL25XPV HEAT PUMP</h2>
+                                <ul>
+                                    <li>Stages of Cooling: Variable-Capacity Operation</li>
+                                    <li>Energy Efficiency 24</li>
+                                    <li>ENERGY STAR®</li>
+                                    <li>Sound Level As low as 58 dBA</li>
+                                    <li>SilentComfort Technology</li>
+                                    <li>Cabinet PermaGuard Cabinet and SmartHinge Louver Design</li>
+                                    <li>R-410A refrigerant</li>
+                                    <li>Tax Credit Eligible: Yes</li>
+                                    <li>iComfort -enabled technology</li>
+                                    <li>All Dave Lennox Signature® Collection models come with a 10-Year Limited
+                                        Warranty on the compressor and
+                                        covered components.</li>
+                                </ul>
+
+                                <h2>Lennox Model EL22XPV HEAT PUMP</h2>
+                                <ul>
+                                    <li>Stages of Cooling: Variable-Capacity Operation</li>
+                                    <li>22.00 SEER2 and 9.50 HSPF2</li>
+                                    <li>ENERGY STAR®</li>
+                                    <li>Sound Level As low as 61 dBA</li>
+                                    <li>Cabinet PermaGuard Cabinet and SmartHinge Louver Design</li>
+                                    <li>R-410A refrigerant</li>
+                                    <li>Tax Credit Eligible: Yes</li>
+                                    <li>Digitally-enabled technology</li>
+                                    <li>10-Year Limited Warranty on compressor, 5-Year Limited Warranty on covered
+                                        components. This product may be
+                                        eligible for valuable 10-Year Limited Extended Warranty coverage with product
+                                        registration.</li>
+                                </ul>
+
+                                <h2>Lennox Model EL16XP1 HEAT PUMP</h2>
+                                <ul>
+                                    <li>Stages of Cooling: Single Stage</li>
+                                    <li>Up to 17 (SEER), Up to 9.8 (HSPF)</li>
+                                    <li>ENERGY STAR®</li>
+                                    <li>Sound Level As low as 71 dBA</li>
+                                    <li>Cabinet PermaGuard Cabinet</li>
+                                    <li>R-410A refrigerant</li>
+                                    <li>Tax Credit Eligible: Yes</li>
+                                    <li>Elite models come with a 10-Year Limited Warranty on compressors and a 5-Year
+                                        Limited Warranty on covered
+                                        components. A 10-Year Extended Limited Warranty is available with product
+                                        registration.</li>
+                                </ul>
+
+                                <br><br>
+
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
+                                    Quote</a>
+                                <br> <br>
+                                <p><strong>When it comes to getting the best heating and cooling equipment or service,
+                                        the only
+                                        name you need to know in the
+                                        Thumb area of Michigan is Jacks Heating and Cooling.</strong></p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
