60aeb9fccc5505727d967839a72cffb5daad4b9e
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-08-02 13:00:43 +0500

    updated commercial

---
+++ commercial.php
@@ -0,0 +1,79 @@
+<?php include_once('header.php') ?>
+<title>Jacks Heating and Cooling | Commercial</title>
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+                                <img src="<?php echo $base_url ?>/img/jack's 2x2 100x350.png" alt="HVAC rooftop"
+                                    class="full-width-img">
+
+                                <br><br>
+
+                                <p>At Jacks Heating and Cooling, we fully recognize the importance of creating a
+                                    comfortable indoor environment for
+                                    both your employees and customers. With our comprehensive range of systems,
+                                    services, and solutions, we
+                                    guarantee significant savings in HVAC utility costs for your business. Since 1947,
+                                    we have been the go-to
+                                    experts for commercial HVAC installations, specializing in the latest
+                                    energy-efficient systems available,
+                                    regular maintenance, and swift HVAC repairs.</p>
+
+                                <p>Our dedicated team at Jacks Heating and Cooling is well-equipped to cater to your
+                                    specific needs and budget
+                                    requirements. We provide tailored routine maintenance, repairs, and new
+                                    installations of HVAC and VRF systems.
+                                    Our services cover a wide array of commercial equipment, including furnaces,
+                                    boilers, air conditioning units,
+                                    packaged units, and rooftop units. Rest assured, we handle all brands and models of
+                                    commercial heating and air
+                                    conditioning equipment with the utmost expertise and care.</p>
+
+                                <h2>Commercial Services:</h2>
+                                <ul>
+                                    <li>Duct Systems and Modifications</li>
+                                    <li>Ductless Air Conditioning Split Systems (VRF)</li>
+                                    <li>All-in-One Solution Packaged Systems</li>
+                                    <li>Complete seasonal inspections of HVAC systems</li>
+                                    <li>24/7 Emergency Repair Service</li>
+                                    <li>Temperature/Humidity Control</li>
+                                    <li>Indoor Air Quality</li>
+                                    <li>HVAC Sales, Repair, Installation</li>
+                                    <li>Rooftop Units</li>
+                                    <li>HVAC</li>
+                                    <li>Boilers</li>
+                                    <li>Maintenance Plans</li>
+                                </ul>
+
+                                <br>
+
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Contact Us</a>
+
+                                <p><strong>When it comes to getting the best commercial HVAC equipment or service, the only name
+                                    you need to know in the
+                                    Thumb area of Michigan is Jacks Heating and Cooling.</strong></p>
+
+                                <br>
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
