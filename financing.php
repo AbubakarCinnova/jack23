c5bed66eaaa0434f8bcf62adc778e22deed9c533
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-08-02 12:54:37 +0500

    updated financing

---
+++ financing.php
@@ -0,0 +1,53 @@
+<?php include_once('header.php') ?>
+<title>Jacks Heating and Cooling | Financing</title>
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+
+
+                                <h1>Financing</h1>
+
+                                <img src="<?php echo $base_url ?>/img/authorized_contractor-1 (2).png"
+                                    alt="Financing" class="full-width-img">
+
+                                <br>
+
+                                <p>A new home heating and cooling system is one of the most important investments you'll
+                                    make, one that can deliver energy and cost savings for many years to come. You don't
+                                    have to let a small budget keep you from getting the most efficient solution for
+                                    your household. That's why we offer flexible financing options.
+                                </p>
+                                <br>
+                                <img src="<?php echo $base_url ?>/img/credit (1).jpeg"
+                                    alt="---Michigan saves---" class="full-width-img">
+
+                                <br>
+
+                                <p> Financing is subject to credit approval.</p>
+                                <p><strong>Call us today to find out other financing options we have available for you.</strong></p>
+
+                                <br>
+
+                                
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
