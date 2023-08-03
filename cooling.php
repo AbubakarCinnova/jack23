9eaa348bd70d9405fe943f37a9904c358d73b06b
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-08-03 12:01:07 +0500

    Update cooling.php

--- cooling.php
+++ cooling.php
@@ -15,7 +15,7 @@
                         <div class="row">
                             <div class="col-sm-12">
 
-                                <img src="<?php echo $base_url ?>/img/Need_a_new_ac-1 (2) (1).jpg" alt="HVAC rooftop"
+                                <img src="<?php echo $base_url ?>/img/Need_a_new_ac-1 (2) (1).jpg" alt="need ac repair "
                                     class="img-responsive">
 
                                 <br><br>
