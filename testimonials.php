2b7b25fc8827476d9f9b278c2017782b3f2ada1f
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-08-03 11:26:41 +0500

    updated testimonials

---
+++ testimonials.php
@@ -0,0 +1,41 @@
+<?php include_once('header.php') ?>
+<title>Jacks Heating and Cooling | Testimonials</title>
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+                                <br><br>
+
+                                <a
+                                    href="https://www.google.com/search?q=jacks+heating+mi&oq=&aqs=chrome.0.69i59i450l8.1502674897j0j15&sourceid=chrome&ie=UTF-8#lrd=0x88240535403e67d7:0x1314043c5d6182ec,3,,,,&rlimm=1374728443566064364"><img
+                                        src="<?php echo $base_url ?>/img/reviews-google (1) (1).gif" alt="Financing"
+                                        width="300"></a>
+
+                                <br><br>
+
+
+
+                                <br>
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
