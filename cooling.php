8529d336b397973f2ee843c47d96407f232e7b09
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-08-03 11:59:05 +0500

    updated cooling

---
+++ cooling.php
@@ -0,0 +1,126 @@
+<?php include_once('header.php') ?>
+<title>Jacks Heating and Cooling | Cooling</title>
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+                                <img src="<?php echo $base_url ?>/img/Need_a_new_ac-1 (2) (1).jpg" alt="HVAC rooftop"
+                                    class="img-responsive">
+
+                                <br><br>
+
+                                <h2>Air Conditioner Repair Services</h2>
+                                <p>Having trouble with your air conditioning system not effectively cooling your home or
+                                    business? Is your central AC
+                                    unit in need of a recharge or not functioning at all? Don't worry, at Jacks Heating
+                                    and Cooling, our friendly
+                                    and skilled technicians are well-equipped to swiftly diagnose and resolve any
+                                    problems with your air
+                                    conditioning system. We specialize in repairing all brands and types of heating and
+                                    cooling equipment to ensure
+                                    your comfort is restored promptly.</p>
+
+                                <h2>Air Conditioner Maintenance</h2>
+                                <p>Avoid enduring the sweltering Michigan summer inside a hot home. Trust Jack's Heating
+                                    and Cooling for
+                                    professional air conditioning maintenance to prevent any breakdowns in your cooling
+                                    system. Regular AC
+                                    maintenance can extend the lifespan of your central air system and keep your
+                                    expenses in check. Explore our
+                                    maintenance plans at Jack's Heating and Cooling, ensuring a trouble-free summer for
+                                    your HVAC system!</p>
+                                <br>
+
+                                <a href="<?php echo $base_url ?>/tin-man-club.php" class="btn btn-warning">Maintenance
+                                    Plan</a>
+                                <br>
+
+
+                                <h2>High-Efficiency Air Conditioning Systems that Will Save You Money</h2>
+                                <p>Choosing the correct air conditioning system is an important decision for every
+                                    homeowner. Most often, making the
+                                    right selection comes down to three factors: efficiency, comfort, and budget. To
+                                    find out which Lennox model
+                                    delivers the best for your home, call us today for a free estimate and consultation.
+                                    Jacks Heating and Cooling
+                                    comfort designer will schedule a visit to your home. The comfort advisor will
+                                    explain the heating and cooling
+                                    systems available and suitable for your home and lay out all the options to help you
+                                    determine the best balance
+                                    of comfort, efficiency, and cost.</p>
+                                <br>
+
+                                <a href="<?php echo $base_url ?>/air-conditioners.php" class="btn btn-warning">View Air
+                                    Conditioners</a>
+                                <br>
+
+
+                                <h2>Ductless Mini Split</h2>
+                                <p>A ductless mini-split system is an efficient heating and cooling solution that
+                                    delivers conditioned air directly
+                                    to individual rooms in your home without the need for ducts. Also known as
+                                    mini-split, multi-split, heat pumps,
+                                    or ductless air conditioning systems, these systems are perfect for homes without
+                                    traditional ductwork, as well
+                                    as for garages, workshops, and additions. They are a great option if you want to add
+                                    AC in a specific room or
+                                    area without modifying the existing ductwork.</p>
+
+                                <h2>We Offer Sales, Service, and Installation of:</h2>
+                                <ul>
+                                    <li>Air Conditioning Installation</li>
+                                    <li>Residential and Commercial</li>
+                                    <li>New Home Construction Comfort Systems</li>
+                                    <li>HVAC Replacement</li>
+                                    <li>AC recharging</li>
+                                    <li>Air Conditioning Repairs</li>
+                                    <li>High-Efficiency Air Conditioning</li>
+                                    <li>Air Conditioning Repair</li>
+                                    <li>Maintenance Plans</li>
+                                    <li>Ductless Mini Split Systems</li>
+                                    <li>Central Air Repair</li>
+                                    <li>AC Repair</li>
+                                    <li>Duct Cleaning</li>
+                                    <li>Ductwork Repairs and Replacements</li>
+                                    <li>UV Germicidal Lights</li>
+                                    <li>WiFi Thermostats</li>
+                                    <li>Affordable Pricing</li>
+                                    <li>Lennox Dealer</li>
+                                    <li>RUUD Dealer</li>
+                                    <li>Indoor Air Quality Installation</li>
+                                    <li>24/7 Emergency Services</li>
+                                    <li>Whole House Humidifiers</li>
+                                    <li>Whole House Air Purifiers</li>
+                                </ul>
+
+                                <p>When it comes to getting the best heating and cooling equipment or service, the only
+                                    name you need to know is
+                                    Jack's Heating and Cooling.</p>
+
+                                <br>
+
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Contact Us</a>
+                                <br>
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
